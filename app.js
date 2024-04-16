b81db03f69a959822c803e63a127057c0f4536d9
Zorishimo <caseyj77@gmail.com>
2024-04-16 10:08:05 -0500

    changed rsp to a const and moved it out of the fuction

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 // Holding document for js code //
 
+
+// Creating a list of possible options.//
+const rsp = ['rock', 'paper', 'scissors']
+
 // Getting random choice from the computer player.//
-
-
 function getComputerChoice () {
-    let rsp = ['rock', 'paper', 'scissors']
 
     let rand = Math.floor(Math.random() * rsp.length);
     return rsp[rand];
 
 }
+
+// Getting input from the user via a prompt. //
+
