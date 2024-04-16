65c85f7c1188caf8ef4c33d2ebdac9580a2625db
Zorishimo <caseyj77@gmail.com>
2024-04-16 15:06:55 -0500

    fixxed a syntax error, added a mechanism to make the input lowercase, added final score

--- app.js
+++ app.js
@@ -1,49 +1,50 @@
-// Holding document for js code //
+// Creating a list of possible options.
+const rsp = ['rock', 'paper', 'scissors'];
 
-
-// Creating a list of possible options.//
-const rsp = ['rock', 'paper', 'scissors']
-
-// Getting random choice from the computer player.//
-function getComputerChoice () {
-
+// Getting random choice from the computer player.
+function getComputerChoice() {
     let rand = Math.floor(Math.random() * rsp.length);
     return rsp[rand];
-
 }
 
-// Getting input from the user via a prompt. //
+// Getting input from the user via a prompt.
 
-let playerSelection = prompt("type rock, paper, or scissors");
-let computerSelection = getComputerChoice();
-
-// play a round //
+// play a round
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection){
-        return "You both selected " + playerSelection +". The Game is a tie";
+        return "You both selected " + playerSelection + ". The Game is a tie";
     }
-    else if ((playerSelection === 'rock' && computerSelection === 'scissors')||
+    else if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
              (playerSelection === 'paper' && computerSelection === 'rock') ||
-             (playerSelection === "scissors" && computerSelection === 'paper'))
-        return "You win " + playerSelection + " beats " + computerSelection + "."
+             (playerSelection === 'scissors' && computerSelection === 'paper')) {
+        return "You win " + playerSelection + " beats " + computerSelection + ".";
+    }
     else {
-        return "You lose " + computerSelection + " beats " + playerSelection + "."
+        return "You lose " + computerSelection + " beats " + playerSelection + ".";
     }
 }
 
 // play a five round game
-function playGame(){
+function playGame() {
     let playerScore = 0;
-    let computerScore =0;
+    let computerScore = 0;
 
     for(let i = 0; i < 5; i++) {
-        let result = playRound(playerSelection, computerSelection);
-        
-        if (result === "You win " + playerSelection + " beats " + computerSelection + ".") {
+        let playerSelection = prompt("Type rock, paper, or scissors");
+        let computerSelection = getComputerChoice();
+        let result = playRound(playerSelection.toLowerCase(), computerSelection.toLowerCase());
+
+        if (result.includes("You win")) {
             playerScore += 1;
-    }       else if (result === "You lose " + computerSelection + " beats " + playerSelection + ".") {
-            computerScore =+ 1;
-    }
+        } else if (result.includes("You lose")) {
+            computerScore += 1;
+        }
+
         console.log(`Round ${i+1}: ${result}`);
     }
+
+    console.log(`Final Score: Player ${playerScore} - ${computerScore} Computer`);
 }
+
+// Start the game
+playGame();
