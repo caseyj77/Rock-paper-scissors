b819a8f31824a432ce924cf7249b7a7aad9ae21f
Zorishimo <caseyj77@gmail.com>
2024-04-16 09:48:33 -0500

    added getcomputerchoice function

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// Holding document for js code
+// Holding document for js code //
+
+// Getting random choice from the computer player.//
+
+
+function getComputerChoice () {
+    let rsp = ['rock', 'paper', 'scissors']
+
+    let rand = Math.floor(Math.random() * rsp.length);
+    return rsp[rand];
+
+}
